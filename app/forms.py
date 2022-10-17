f3d2a3fddf5bda9c43009fa040486476b2980d1a
Vinicius Groto de oliveira <102178039+vinig79@users.noreply.github.com>
2022-10-17 15:03:20 -0300

    Add files via upload

--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,10 @@ from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 class LoginForm(FlaskForm):
     username = StringField('username',validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(),Email()])
-    subimit = StringField()
+    submit = SubmitField("Submit")
 
 class ResgistrationForm(FlaskForm):
     username = StringField('username',validators=[DataRequired()])
     email = StringField('email',validators=[DataRequired(), Email()])
     email2 = StringField('email',validators=[DataRequired(), Email(),EqualTo('email')])
-    subimit = SubmitField()
+    subimit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -7,14 +7,16 @@ from random import randint
 @app.route('/')
 @app.route('/index')
 def index():
+
     def retorno():
         request_pokemon = requests.get('http://pokeapi.co/api/v2/pokemon/')
         pokemons = request_pokemon.json()['results']
         pokemon = requests.get(pokemons[randint(0, len(pokemons))]['url']).json()['sprites']['back_default']
         return pokemon
+
     return render_template('index.html', title="Pokemon Game",imagem=retorno())
 
-'''@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title="login")'''
+    return render_template('login.html', title="login", form=form)
