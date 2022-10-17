01e480943c63a6d04ccbe2501b39cd61f3a39d94
Vinicius Groto de oliveira <102178039+vinig79@users.noreply.github.com>
2022-10-17 10:00:32 -0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-class Config ():
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root@localhost/pokemon_game'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI =  'mysql+mysqlconnector://vinig79:vinig79@localhost/pokemon_game'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
